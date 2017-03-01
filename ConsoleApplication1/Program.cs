6eb629dd6b61b0577670a96b6514b888b3c667bb
Akshay Sharma <akshay_sharma@optum.com>
2017-03-01 23:27:46 +0530

    line removed

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
         {
             //this is the where the coding starts 
             //this is console application, code to be written in C#
-            //meeting to be started
+         
 
         }
     }
